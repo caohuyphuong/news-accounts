a42c4b6762a5d665ae4db550d3ecffcd86f22896
Cao <caohuyphuong@gmail.com>
2021-09-20 11:10:10 +0700

    Part 2 - Model

--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = 'Category'
+
+    def __str__(self):
+        return self.title
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Blog'
+
+    def __str__(self):
+        return self.title
