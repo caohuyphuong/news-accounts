c7d166ca6dcf0aaea99bd13f55b73b7d04845765
Cao <caohuyphuong@gmail.com>
2021-10-16 21:55:54 +0700

    Part 20 accounts - User registration

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -5,8 +5,10 @@
 
 from django.contrib.auth import views
 from django.urls import path, reverse_lazy
+from .views import UserRegistrationView
 app_name = 'accounts'
 urlpatterns = [
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('login/', views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', views.LogoutView.as_view(template_name='accounts/logged_out.html'), name='logout'),
 
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.utils.crypto import get_random_string
+from django.core.mail import send_mail
+from accounts.forms import UserRegistrationForm
 
-# Create your views here.
+
+class UserRegistrationView(generic.CreateView):
+    template_name = 'accounts/registration.html'
+    form_class = UserRegistrationForm
+    success_url = reverse_lazy('accounts:login')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        user.set_password(get_random_string(length=8))
+        user.save()
+        send_mail(
+            subject="Thank you for registration",
+            message="We've received your registration. We will test and reply you soon.",
+            from_email="admin@email.com",
+            recipient_list=[user.email, ]
+        )
+        return super().form_valid(form)
